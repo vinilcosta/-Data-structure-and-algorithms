2aba406cf90037211215b13cd026823449117543
Marcos Vinicius <marcosvlcosta8@gmail.com>
2022-10-03 22:47:44 -0300

    Foi adicionado o método values.

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
+class Set {
+  constructor() {
+    this.items = {};
+  }
 
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
 
-class Set{
-    constructor(){
-        this.items = {};
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element;
+      return true;
     }
+    return false;
+  }
 
-    has(element){
-        return Object.prototype.hasOwnProperty.call(this.items, element);
-     }
-
-    add(element){ 
-        if(!this.has(element)){
-            this.items[element]= element;
-            return true;
-        }
-        return false;
+  Delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
+      return true;
     }
+    return false;
+  }
 
-    Delete(element){
-        if(this.has(element)){
-            delete this.items[element];
-            return true;
-        }
-        return false
-     }
+  clear() {
+    this.items = {};
+  }
 
-    clear(){ 
-        this.items ={};
-    }
-
-   size(){
+  size() {
     let count = 0;
-    for(let key in this.items){
-        if(this.items.hasOwnProperty(key)){
-            count++;
-        }
+    for (let key in this.items) {
+      if (this.items.hasOwnProperty(key)) {
+        count++;
+      }
     }
     return count;
-   }
+  }
 
-   values(){}
-
-
+  values() {
+    let values=[];
+    for(let key in this.items){
+        if(this.items.hasOwnProperty(key)){
+            values.push(key);
+        }
+    }
+    return values;
+  }
 }
